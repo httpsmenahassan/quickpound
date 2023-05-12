91de0a0caa9c77c62d8169dccbeee2bf99dbfa4f
httpsmenahassan <whobutmenacodes@gmail.com>
2023-05-12 12:16:18 -0400

    first commit

---
+++ middleware/multer.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const path = require("path");
+
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    // .toLowerCase()
+    // multer can handle pretty much any file type, can just change the accepted files here and will need to check if cloundinary can handle that file type as well 
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      cb(new Error("File type is not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
